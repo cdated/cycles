57e23052c663472f61f08ad118654167cb8d3729
Brandon T. Fields <brandon@cdated.com>
2025-01-25 22:28:38 -0500

    feat: connect to db

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/cdated/cycles/events"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
-func main() {
+func eventOps() {
 	fmt.Println("Create event")
 	e1 := events.NewEventRecurs("Use device a", time.Second)
 	fmt.Println(e1)
@@ -27,4 +32,21 @@ func main() {
 
 	e2 := events.NewEvent("Use device b")
 	fmt.Println(e2)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DB_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	// Add sample events
+	eventOps()
 }
